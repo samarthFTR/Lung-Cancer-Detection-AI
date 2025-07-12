05a721b372cec34fa0979f277c9449812063818a
samarthFTR <srivastavasamarth69@gmail.com>
2025-07-12 12:09:13 +0530

    Fixed Custom exception

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,7 +3,10 @@ import sys
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -35,7 +38,10 @@ class DataIngestion:
                  self.ingestion_config.test_data_path,
              )
          except Exception as e:
-             pass
+             raise CustomException(e,sys)
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    DataTransformation=DataTransformation()
+    train_arr,test_arr,_=DataTransformation.initiate_data_transformation(train_data,test_data)
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -8,6 +8,9 @@ from sklearn.impute import SimpleImputer # empty data handelling
 from sklearn.preprocessing import OneHotEncoder,StandardScaler #Encoding and standardiztion
 import os
 
+import sys
+from src.exception import CustomException
+from src.utils import save_object
 from src.logger import logging
 
 @dataclass
@@ -15,12 +18,99 @@ class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('data','preprocessor.pkl')
 
 class DataTransformation:
-    def __init__(self):
-        self.datatransformationconfig = DataTransformationConfig()
+    """
 
-        def get_data_tranformer_object(self): #for encoding and standardization
-            try:
-                pass
-            except:
-                pass
-            
+    This function is used for data tranfromation
+
+    """
+    def __init__(self):
+        self.DataTransformationConfig = DataTransformationConfig()
+
+    def get_data_tranformer_object(self): #for encoding and standardization
+        try:
+            numerical_columns=['Age']
+            categorical_columns=[
+                'Gender', 'Air Pollution','Alcohol use',
+                'Dust Allergy', 'OccuPational Hazards', 'Genetic Risk',
+                'chronic Lung Disease', 'Balanced Diet', 'Obesity', 'Smoking',
+                'Passive Smoker', 'Chest Pain', 'Coughing of Blood', 'Fatigue',
+                'Weight Loss', 'Shortness of Breath', 'Wheezing',
+                'Swallowing Difficulty', 'Clubbing of Finger Nails',
+                'Frequent Cold','Dry Cough', 'Snoring'
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ('imputer',SimpleImputer(strategy="median")),
+                    ('scalar',StandardScaler()),
+                ]
+            )
+            cat_pipeline = Pipeline(
+                steps=[
+                    ('imputer',SimpleImputer(strategy='most_frequent')),
+                    ('OneHotEncoder',OneHotEncoder()),
+                    ('scalar',StandardScaler(with_mean=False)),
+                ]
+            )
+            logging.info(f'Categorical columns: {categorical_columns}')
+            logging.info(f'Numerical columns: {numerical_columns}')
+
+
+            preprocessor=ColumnTransformer(
+                [
+                    ('num_pipeline',num_pipeline,numerical_columns),
+                    ('cat_pipeline',cat_pipeline,categorical_columns),
+                ]
+            )
+
+            return preprocessor
+        except Exception as e:
+            raise CustomException(e,sys)
+
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+           train_df=pd.read_csv(train_path)
+           test_df=pd.read_csv(test_path)
+
+           logging.info('Reading test and train data completed')
+           logging.info('Obtaining Preprocessing Object')
+           
+           preprocessor_obj = self.get_data_tranformer_object()
+           
+           target_column = 'Level'
+           indexing_colums = ['','index']
+
+           input_feature_train_df=train_df.drop(columns=[target_column],axis=1)
+           target_feature_train_df=train_df[target_column]
+
+           input_feature_test_df=test_df.drop(columns=[target_column],axis=1)
+           target_feature_test_df=test_df[target_column]
+
+           logging.info(
+               f"Applying preprocessor object on training and test dataframe"
+           )
+
+           input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
+           input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
+
+           train_arr=np.c_[
+               input_feature_train_arr,np.array(target_feature_train_df)
+           ]
+           test_arr=np.c_[
+               input_feature_test_arr,np.array(target_feature_test_df)
+           ]
+
+           logging.info('saving preprocessing object.')
+
+           save_object(
+                file_path = self.DataTransformationConfig.preprocessor_obj_file_path,
+                obj=preprocessor_obj
+            ) #to save pickle file
+
+           return(
+               train_arr,
+               test_arr,
+               self.DataTransformationConfig.preprocessor_obj_file_path,
+           )
+        except Exception as e:
+            raise CustomException(e,sys)
