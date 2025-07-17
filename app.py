6ce02f97a86cf3715b2eb495c030284ab3528661
samarthFTR <srivastavasamarth69@gmail.com>
2025-07-17 21:32:24 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 
 application=Flask(__name__)
@@ -17,6 +18,39 @@ def index():
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
-        return render_template('home.html')
+        return render_template('index.html')
     else:
-        pass
+        data=CustomData(
+            Age=int(request.form.get('age')),  # Numerical data
+            Gender=request.form.get('gender'),  # Categorical
+            Air_Pollution=request.form.get('air_pollution'),  # Categorical
+            Alcohol_use=request.form.get('alcohol_use'),  # Categorical
+            Dust_Allergy=request.form.get('dust_allergy'),  # Categorical
+            OccuPational_Hazards=request.form.get('occupational_hazards'),  # Categorical
+            Genetic_Risk=request.form.get('genetic_risk'),  # Categorical
+            chronic_Lung_Disease=request.form.get('chronic_lung_disease'),  # Categorical
+            Balanced_Diet=request.form.get('balanced_diet'),  # Categorical
+            Obesity=request.form.get('obesity'),  # Categorical
+            Smoking=request.form.get('smoking'),  # Categorical
+            PassiveSmoker=request.form.get('passive_smoker'),  # Categorical
+            ChestPain=request.form.get('chest_pain'),  # Categorical
+            CoughingofBlood=request.form.get('coughing_of_blood'),  # Categorical
+            Fatigue=request.form.get('fatigue'),  # Categorical
+            WeightLoss=request.form.get('weight_loss'),  # Categorical
+            ShortnessofBreath=request.form.get('shortness_of_breath'),  # Categorical
+            Wheezing=request.form.get('wheezing'),  # Categorical
+            SwallowingDifficulty=request.form.get('swallowing_difficulty'),  # Categorical
+            ClubbingofFingerNails=request.form.get('clubbing_of_finger_nails'),  # Categorical
+            FrequentCold=request.form.get('frequent_cold'),  # Categorical
+            DryCough=request.form.get('dry_cough'),  # Categorical
+            Snoring=request.form.get('snoring')  # Categorical
+        )
+        pred_df=data.get_data_as_frame()
+        print(pred_df)
+
+        Predict_Pipeline=PredictPipeline()
+        results = Predict_Pipeline.predict(pred_df)
+        return render_template('index.html',results=results[0])
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,84 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+    def predict(self,features):
+        try:
+            model_path = 'data\model.pkl'
+            preprocessor_path = 'data\preprocessor.pkl'
+            model=load_object(model_path)
+            preprocessor=load_object(preprocessor_path)
+            data_scaled = preprocessor.tranform(features)
+            preds = model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+class CustomData:
+    def __init__(
+        self,Gender,Air_Pollution,Alcohol_use,
+        Dust_Allergy,OccuPational_Hazards,Genetic_Risk,
+        chronic_Lung_Disease,Balanced_Diet,Obesity,
+        Smoking,PassiveSmoker,ChestPain,CoughingofBlood,
+        Fatigue,WeightLoss,ShortnessofBreath,Wheezing,
+        SwallowingDifficulty,ClubbingofFingerNails,FrequentCold,
+        DryCough,Snoring,Age):
+        self.Gender = Gender
+        self.Air_Pollution = Air_Pollution
+        self.Alcohol_use = Alcohol_use
+        self.Dust_Allergy = Dust_Allergy
+        self.OccuPational_Hazards = OccuPational_Hazards
+        self.Genetic_Risk = Genetic_Risk
+        self.chronic_Lung_Disease = chronic_Lung_Disease
+        self.Balanced_Diet = Balanced_Diet
+        self.Obesity = Obesity
+        self.Smoking = Smoking
+        self.PassiveSmoker = PassiveSmoker
+        self.ChestPain = ChestPain
+        self.CoughingofBlood = CoughingofBlood
+        self.Fatigue = Fatigue
+        self.WeightLoss = WeightLoss
+        self.ShortnessofBreath = ShortnessofBreath
+        self.Wheezing = Wheezing
+        self.SwallowingDifficulty = SwallowingDifficulty
+        self.ClubbingofFingerNails = ClubbingofFingerNails
+        self.FrequentCold = FrequentCold
+        self.DryCough = DryCough
+        self.Snoring = Snoring
+        self.Age = Age
+
+    def get_data_as_frame(self):
+        try:
+            custom_data_input_dict = {
+                "Age": [self.Age],
+                "Gender": [self.Gender],
+                "Air Pollution": [self.Air_Pollution],
+                "Alcohol use": [self.Alcohol_use],
+                "Dust Allergy": [self.Dust_Allergy],
+                "OccuPational Hazards": [self.OccuPational_Hazards],
+                "Genetic Risk": [self.Genetic_Risk],
+                "chronic Lung Disease": [self.chronic_Lung_Disease],
+                "Balanced Diet": [self.Balanced_Diet],
+                "Obesity": [self.Obesity],
+                "Smoking": [self.Smoking],
+                "Passive Smoker": [self.PassiveSmoker],
+                "Chest Pain": [self.ChestPain],
+                "Coughing of Blood": [self.CoughingofBlood],
+                "Fatigue": [self.Fatigue],
+                "Weight Loss": [self.WeightLoss],
+                "Shortness of Breath": [self.ShortnessofBreath],
+                "Wheezing": [self.Wheezing],
+                "Swallowing Difficulty": [self.SwallowingDifficulty],
+                "Clubbing of Finger Nails": [self.ClubbingofFingerNails],
+                "Frequent Cold": [self.FrequentCold],
+                "Dry Cough": [self.DryCough],
+                "Snoring": [self.Snoring]
+            }
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            raise CustomException(e,sys)
